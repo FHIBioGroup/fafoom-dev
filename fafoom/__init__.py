0f588ab47a3f3e7ad6f337e6141fa529e11de466
maksimovdmitrii <maksimov@fhi-berlin.mpg.de>
2020-04-21 13:09:44 +0200

    Everythong is deleted, directory is empty, start from there.

--- fafoom/__init__.py
+++
@@ -1,9 +0,0 @@
-from structure import MoleculeDescription, Structure
-from genetic_operations import selection, crossover_single_point, crossover_random_points
-from pyaims import AimsObject
-#from pyff import FFObject
-from pynwchem import NWChemObject
-from pyorca import OrcaObject
-from pyforcefield import FFobject
-from utilities import *
-from run_utilities import *
